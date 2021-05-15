a1758e1cf4a22cfced5ec1cbcc1f89eb12118b6a
HarisaranG <harithiru2018@gmail.com>
2021-05-16 00:35:14 +0530

    Updated new page with custom map

---
+++ src/components/customized/MapMarker.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MapMarker() {
+  return (
+    <div>
+      <img height="30em" width="30em" src="images/icons/marker.png" alt="marker" />
+    </div>
+  )
+}
