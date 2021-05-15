3f3a406484ac860860e3c256f57cf32d1763ec7f
HarisaranG <harithiru2018@gmail.com>
2021-05-15 17:46:17 +0530

    Added typesense config and updated search component

---
+++ src/components/TypesenseConfig.js
@@ -0,0 +1,27 @@
+import TypesenseInstantSearchAdapter from 'typesense-instantsearch-adapter';
+
+const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
+  server: {
+    apiKey: 'xyz',
+    nodes: [
+      {
+        host: 'localhost',
+        port: '8109',
+        protocol: 'https',
+      },
+    ],
+  },
+  additionalSearchParameters: {
+    queryBy: 'city_facet, street_name, project_name',
+    queryByWeights: '2,1,1',
+    dropTokensThreshold: 2,
+    typoTokensThreshold: 2,
+    useServerSideSearchCache: true,
+    numTypos: 1,
+  },
+  geoLocationField: 'location',
+});
+
+const searchClient = typesenseInstantsearchAdapter.searchClient;
+
+export default searchClient;
