ae1b5cd97fecdb9186d9c690307c8b855e3cfc84
Sabesh Bharathi <sabesh5601@gmail.com>
2021-05-17 12:39:25 +0530

    Added memojis in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,10 @@ function Footer() {
     <div className="container-fluid footer text-center">
       <h5 className="footer-text">Developed with <img height="30em" src="images/icons/customheart.png" alt="❤️" /> by <a className="unstyled" href="https://github.com/HarisaranG" target="_blank" rel="noopener">Harisaran</a> and <a className="unstyled" href="https://github.com/programVeins" target="_blank" rel="noopener">Sabesh</a></h5>
 
+      <div className="memoji-row">
+        <img className="memoji" src="images/memojis/hari_memoji.png" alt="Sabesh's Memoji" />
+        <img className="memoji" src="images/memojis/sabesh_memoji.png" alt="Sabesh's Memoji" />
+      </div>
 
       <p className="footer-text-bottom">&copy;{new Date().getFullYear()} Property Portal</p>
     </div>);
